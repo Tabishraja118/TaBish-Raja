6d33932ee1435ba30b3fd4408b84be1c52e58b52
Tabishraja118 <rjk19925@gmail.com>
2024-10-05 03:50:33 +0500

    Create Main.py
    TABII'W RAJA

---
+++ Main.py
@@ -0,0 +1,7 @@
+echo "# TaBish-Alone" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Tabishraja118/TaBish-Alone.git
+git push -u origin main
